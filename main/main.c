ae4f295c4db001ad666c9cef4959b4f2e39bb16d
cyoung <cyoung@gmail.com>
2017-02-15 17:02:50 -0500

    Change Wi-Fi storage mode from RAM to FLASH.

--- main/main.c
+++ main/main.c
@@ -78,7 +78,7 @@ static void initialize_wifi(void) {
 	ESP_ERROR_CHECK( esp_event_loop_init(event_handler, NULL) );
 	wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
 	ESP_ERROR_CHECK( esp_wifi_init(&cfg) );
-	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_RAM) );
+	ESP_ERROR_CHECK( esp_wifi_set_storage(WIFI_STORAGE_FLASH) );
 	ESP_ERROR_CHECK( esp_wifi_set_mode(WIFI_MODE_STA) );
 	wifi_config_t sta_config = {
 		.sta = {
